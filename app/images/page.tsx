e280663e4c88eb0fa68e37205fe6af1eef21b93e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-09 18:30:42 +0900

    feat: image group add

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -4,13 +4,23 @@ import { Image } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import React from 'react'
 
+const Images = () => {
+  return (
+    <group>
+      <Image url="./photo.jpg" scale={[4, 3, 1]} position={[1, 1, 1]} a />
+      <Image url="./photo.jpg" scale={[1, 3, 1]} position={[-1, -1, 1]} />
+      <Image url="./photo.jpg" scale={[1.4, 1, 1]} position={[-1, 2, 0]} />
+    </group>
+  )
+}
+
 const Page = () => {
   return (
-    <>
+    <div className="h-dvh">
       <Canvas>
-        <Image url="./photo.jpg" scale={2}></Image>
+        <Images />
       </Canvas>
-    </>
+    </div>
   )
 }
 
