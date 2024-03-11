c2c0536df39891e2bc7dacca70baff35ab4d895e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2024-03-11 18:51:26 +0900

    feat: useAspect add

--- app/images/page.tsx
+++ app/images/page.tsx
@@ -1,12 +1,24 @@
 'use client'
 
-import { Image, Scroll, ScrollControls, useScroll } from '@react-three/drei'
+import {
+  Image,
+  Scroll,
+  ScrollControls,
+  useAspect,
+  useScroll
+} from '@react-three/drei'
 import { Canvas, useFrame } from '@react-three/fiber'
 import React, { useRef } from 'react'
 
 const Images = () => {
   const group = useRef(null)
   const scrollDate = useScroll()
+
+  const scale = useAspect(
+    1900, // Pixel-width
+    500, // Pixel-height
+    1 - 100 / 150 // Optional scaling factor
+  )
 
   useFrame(() => {
     group.current.children[0].material.zoom = 1 + scrollDate.range(0, 1 / 3) / 3
